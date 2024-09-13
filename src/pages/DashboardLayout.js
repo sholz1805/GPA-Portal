16cb26b248fd8923cbc85ecbf4b8f14ce6df6b62
Yetife <tifetreats21@gmail.com>
2024-09-13 19:02:30 +0100

    mobile view fix

--- src/pages/DashboardLayout.js
+++ src/pages/DashboardLayout.js
@@ -10,12 +10,13 @@ const DashboardLayout = ({children}) => {
     }
 
     return (
-        <div className="flex bg-[#EAEEF6]">
+        <div className="flex">
+            {/*<div className={`fixed z-20 transition-opacity bg-black lg:hidden ${ menuOpen ? 'opacity-50 block' : 'opacity-0'} `} ></div>*/}
             <Sidebar openSidebar={menuOpen}
                      updateSidebarOpen={updateSidebarOpen}/>
             <div className="flex-grow">
                 <Navbar openSidebar={updateSidebarOpen}/>
-                <main className='md:pr-20 flex-1 w-full'>
+                <main className=' flex-1 w-full bg-[#EAEEF6]'>
                     <div className="px-6 py-6 pt-28 mx-auto w-full">
                         <div className='w-full pt-2 lg:w-[calc(100%) - 18rem]'>
                             {children}
--- src/pages/dashboard/Overview.js
+++ src/pages/dashboard/Overview.js
@@ -1,14 +1,138 @@
-import React from 'react';
+import React from "react";
 import DashboardLayout from "../DashboardLayout";
+import Card from "../../components/Card";
+import {
+  RiFlowChart,
+  RiTeamLine,
+  RiCashLine,
+  RiGitBranchLine,
+  RiArrowDownSLine ,
+} from "react-icons/ri";
+import DoughnutChart from "./ChartData/DoughnutChart";
+import ChartsTable from "./ChartData/ChartTable";
+
+const formatNumber = (number, isCurrency = false) => {
+  let formattedNumber = "";
+
+  if (number >= 1000000) {
+    formattedNumber = (number / 1000000).toFixed(1) + "m";
+  } else if (number >= 10000) {
+    formattedNumber = (number / 1000).toFixed(1) + "k";
+  } else {
+    formattedNumber = number.toString();
+  }
+
+  if (isCurrency) {
+    return `N${formattedNumber}`;
+  }
+
+  return formattedNumber;
+};
+
+const cardDetails = [
+  {
+    Icon: RiTeamLine,
+    percentage: "-0.6%",
+    number: 67500,
+    cardLabel: "Total Members",
+  },
+  {
+    Icon: RiFlowChart,
+    percentage: "1.2%",
+    number: 6,
+    cardLabel: "Total Zones",
+  },
+  {
+    Icon: RiGitBranchLine,
+    percentage: "0.8%",
+    number: 65,
+    cardLabel: "Total Branches",
+  },
+  {
+    Icon: RiCashLine,
+    percentage: "2.5%",
+    number: 5100000,
+    cardLabel: "Total Income",
+    isCurrency: true,
+  },
+];
 
 const Overview = () => {
-    return (
-        <DashboardLayout>
-            <div className="text-[24px] font-[600] text-[#0D0D0D]">
-                Overview
+  return (
+    <DashboardLayout>
+
+      <div className='overflow-auto custom-scroll-bar'>
+        <div className="text-[24px] font-[600] text-[#0D0D0D]">Overview</div>
+        <div className="flex justify-center mt-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 max-w-screen-xl w-full px-4">
+            {cardDetails.map((details, index) => (
+                <Card
+                    key={index}
+                    Icon={details.Icon}
+                    percentage={details.percentage}
+                    number={formatNumber(details.number, details.isCurrency)}
+                    cardLabel={details.cardLabel}
+                />
+            ))}
+          </div>
+        </div>
+
+        <div className="flex flex-col lg:flex-row justify-center mt-5">
+          <div className="w-full lg:max-w-screen-xl bg-transparent px-4">
+            <div className="flex flex-col lg:flex-row gap-4">
+              <div className="w-full lg:w-[63%] flex flex-col gap-4">
+                <div
+                    className="border border-[#D9D9D9] bg-white rounded-lg p-4 flex flex-col justify-between"
+                    style={{ height: "300px" }}
+                >
+                  <div className="border-b border-[#D9D9D9] p-2 bg-green flex justify-between">
+                    <p className="font-medium text-base">
+                      Zonal Members breakdown
+                    </p>
+                    <button className="border border-[#D9D9D9] cursor-pointer p-2 rounded-lg flex justify-between items-center space-x-2">
+                      <span >All time</span>
+                      <RiArrowDownSLine  className="text-[#0D0D0D] text-m" />
+                    </button>
+                  </div>
+                  <div className="flex-1 p-1 flex justify-between items-center">
+                    <DoughnutChart />
+                    <ChartsTable/>
+                  </div>
+                </div>
+
+                <div
+                    className="border border-[#D9D9D9] bg-white rounded-lg p-4 flex flex-col justify-between"
+                    style={{ height: "300px" }}
+                >
+                  <div className="border-b border-[#D9D9D9] p-2 bg-green flex justify-between">
+                    <p className="font-medium text-base">
+                      Zonal financial breakdown
+                    </p>
+                    <button className="border border-[#D9D9D9] cursor-pointer p-2 rounded-lg flex justify-between items-center space-x-2">
+                      <span >All time</span>
+                      <RiArrowDownSLine  className="text-[#0D0D0D] text-m" />
+                    </button>
+                  </div>
+                  <div className="flex-1 p-1 flex justify-between items-center">
+                    <DoughnutChart />
+                    <ChartsTable/>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                  className="w-full lg:w-[37%] border border-[#D9D9D9] bg-white rounded-lg p-4 flex flex-col justify-between"
+                  style={{ height: "600px" }}
+              >
+                <p className="font-bold text-xl">Content 2</p>
+                <p>Additional content goes here</p>
+              </div>
             </div>
-        </DashboardLayout>
-    );
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Overview;
